6d602ea9928c0f0a5c942feb5e0af7ab3b535b65
Narendra@523116 <nalamothunarendra@gmail.com>
2024-08-04 16:45:40 +0530

    fixing the errors in the explore options which is of irregular alignments

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,6 @@ document.addEventListener('DOMContentLoaded',  function(){
             let data = according.querySelector(".according-data");
             let icon = according.querySelector(".icon-rotate");
 
-            console.log(data);
-            console.log(icon);
-
             data.classList.toggle("show");
             icon.classList.toggle("animate");
         });
